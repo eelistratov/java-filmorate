d6a978424f5af7efc3e06cff13fc70221151b7fd
Scooter <Scooter7@mail.ru>
2026-06-24 09:07:35 +0300

    Исправление замечаний проверки гита 3

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -14,7 +14,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;

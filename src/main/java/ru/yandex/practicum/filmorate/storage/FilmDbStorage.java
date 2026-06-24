c8f8dcea68cad962dd66e53e54c3d0e887712873
Scooter <Scooter7@mail.ru>
2026-06-24 09:03:08 +0300

    Исправление замечаний проверки гита

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -17,7 +17,6 @@ import ru.yandex.practicum.filmorate.model.MpaRating;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.*;
 
 @Slf4j
